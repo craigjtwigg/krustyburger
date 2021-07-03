67bdb92c247cf1c62aef8d16fe1cc862f6221a66
craigjtwigg <81586983+craigjtwigg@users.noreply.github.com>
2021-07-03 12:53:56 +0100

    inital commit

---
+++ dist/index.main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Welcome to Krusty Burger!")
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9rcnVzdHlidXJnZXIvLi9zcmMvaW5kZXguanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSx3QyIsImZpbGUiOiJpbmRleC5tYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJXZWxjb21lIHRvIEtydXN0eSBCdXJnZXIhXCIpIl0sInNvdXJjZVJvb3QiOiIifQ==
